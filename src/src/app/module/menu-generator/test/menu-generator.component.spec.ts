1894e61d03a8cf5b96d97cb4534ac545947386c0
Guilherme Camargo <g.camargo@outlook.com>
2023-05-15 18:45:08 -0300

    change build script

--- src/src/app/module/menu-generator/test/menu-generator.component.spec.ts
+++ src/src/app/module/menu-generator/test/menu-generator.component.spec.ts
@@ -6,40 +6,42 @@ import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MerchantService } from '../../service/merchant.service';
 import { MenuGeneratorComponent } from '../menu-generator.component';
-import { routes } from '../../../app-routing.module'
+import { routes } from '../../../app-routing.module';
 import { By } from '@angular/platform-browser';
 
 describe('MenuGeneratorComponent', () => {
-  let router: Router
-  let location: Location
+  let router: Router;
+  let location: Location;
   let component: MenuGeneratorComponent;
   let fixture: ComponentFixture<MenuGeneratorComponent>;
 
   let merchantService = jasmine.createSpyObj('MerchantService', {
-    setMerchant: null
-  })
+    setMerchant: null,
+  });
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule.withRoutes(routes)],
       declarations: [MenuGeneratorComponent],
-      providers: [{ provide: MerchantService, useValue: merchantService }, FormBuilder],
-      schemas: [NO_ERRORS_SCHEMA]
-    })
-      .compileComponents();
+      providers: [
+        { provide: MerchantService, useValue: merchantService },
+        FormBuilder,
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
 
-    router = TestBed.inject(Router)
-    location = TestBed.inject(Location)
+    router = TestBed.inject(Router);
+    location = TestBed.inject(Location);
 
     fixture = TestBed.createComponent(MenuGeneratorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
 
-    router.initialNavigation()
+    router.initialNavigation();
   });
 
   function detectChanges(): void {
-    fixture.detectChanges()
+    fixture.detectChanges();
   }
 
   it('should create', () => {
@@ -47,48 +49,52 @@ describe('MenuGeneratorComponent', () => {
   });
 
   it('should thrown error if json is invalid', () => {
-    const JSON_INVALID: string = "{@!"
-    component.getFormControls.json.setValue(JSON_INVALID)
+    const JSON_INVALID: string = '{@!';
+    component.formControls.json.setValue(JSON_INVALID);
 
     expect(() => {
-      component.onSubmit()
-    }).toThrowError()
-  })
+      component.onSubmit();
+    }).toThrowError();
+  });
 
   it('button should be disabled if json is empty', () => {
-    const EMPTY: string = ""
-    component.getFormControls.json.setValue(EMPTY)
-    detectChanges()
+    const EMPTY: string = '';
+    component.formControls.json.setValue(EMPTY);
+    detectChanges();
 
-    const button: HTMLButtonElement = fixture.debugElement.query(By.css(".btn-validate")).nativeElement
-    expect(button.disabled).toBeTrue()
-  })
+    const button: HTMLButtonElement = fixture.debugElement.query(
+      By.css('.btn-validate')
+    ).nativeElement;
+    expect(button.disabled).toBeTrue();
+  });
 
   it('button should be enable if not empty', () => {
-    const CONTENT: string = "{}"
-    component.getFormControls.json.setValue(CONTENT)
-    detectChanges()
+    const CONTENT: string = '{}';
+    component.formControls.json.setValue(CONTENT);
+    detectChanges();
 
-    const button: HTMLButtonElement = fixture.debugElement.query(By.css(".btn-validate")).nativeElement
-    expect(button.disabled).toBeFalse()
-  })
+    const button: HTMLButtonElement = fixture.debugElement.query(
+      By.css('.btn-validate')
+    ).nativeElement;
+    expect(button.disabled).toBeFalse();
+  });
 
   it(`should navigate to Menu`, fakeAsync(() => {
-    const menuPath: string = component.MENU_PATH
+    const menuPath: string = component.MENU_PATH;
 
     router.navigateByUrl(menuPath).then(() => {
       expect(location.path()).toBe(menuPath);
     });
-  }))
+  }));
 
-  it("should json to be invalid if value is null or undefined", () => {
-    const controlJson = component.form.get('json')
-    controlJson?.setValue(null)
-    detectChanges()
-    expect(controlJson?.valid).toBeFalsy()
+  it('should json to be invalid if value is null or undefined', () => {
+    const controlJson = component.form.get('json');
+    controlJson?.setValue(null);
+    detectChanges();
+    expect(controlJson?.valid).toBeFalsy();
 
-    controlJson?.setValue(undefined)
-    detectChanges()
-    expect(controlJson?.valid).toBeFalsy()
-  })
+    controlJson?.setValue(undefined);
+    detectChanges();
+    expect(controlJson?.valid).toBeFalsy();
+  });
 });
